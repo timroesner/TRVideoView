7a595d314880074f06bfa613802fc92d6ebbd1d5
timroesner <timroesner@me.com>
2017-09-21 10:17:21 -0700

    Initial Commit

---
+++ TRVideoView/TRVideoView.h
@@ -0,0 +1,19 @@
+//
+//  TRVideoView.h
+//  TRVideoView
+//
+//  Created by Tim Roesner on 9/21/17.
+//  Copyright © 2017 Tim Roesner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TRVideoView.
+FOUNDATION_EXPORT double TRVideoViewVersionNumber;
+
+//! Project version string for TRVideoView.
+FOUNDATION_EXPORT const unsigned char TRVideoViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TRVideoView/PublicHeader.h>
+
+
