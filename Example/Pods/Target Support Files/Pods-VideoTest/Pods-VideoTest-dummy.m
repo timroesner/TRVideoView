971077ad69342c1fdc669fc132dbf126d9689831
timroesner <13894518+timroesner@users.noreply.github.com>
2017-12-08 13:31:30 -0800

    Removed example to reset lingustics detection

--- Example/Pods/Target Support Files/Pods-VideoTest/Pods-VideoTest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VideoTest : NSObject
-@end
-@implementation PodsDummy_Pods_VideoTest
-@end
--- Example/Pods/Target Support Files/TRVideoView/TRVideoView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TRVideoView : NSObject
-@end
-@implementation PodsDummy_TRVideoView
-@end
